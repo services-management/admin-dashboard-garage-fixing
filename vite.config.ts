effa736b7a45b898565e756e26e958d89e47d637
reaksa-maii <maireaska@gmail.com>
2025-10-29 22:07:12 +0700

    update pipline build deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    allowedHosts: ['garas-admin.domrey.online']
+  },
   plugins: [react()],
 })

00c19a75d3bd027ad38c0d1ba21f4bc46a88f422
reaksa-maii <maireaska@gmail.com>
2025-11-16 13:37:34 +0700

    remove code complex task

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -21,9 +21,4 @@ export default function AuthLayout() {
     </div>
   );
 }
-    <div className="flex items-center justify-center min-h-screen bg-[#8b1d13]">
-      <Outlet />
-    </div>
-  );
-}
 

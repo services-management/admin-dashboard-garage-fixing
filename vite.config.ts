c69b2f1595fced816944e95601940eff3bd69d93
reaksa-maii <maireaska@gmail.com>
2025-11-25 00:27:32 +0700

    update install tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 // import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -9,5 +10,5 @@ export default defineConfig({
     port: 3000,
     allowedHosts: ['garas-admin.domrey.online']
   },
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
 })
